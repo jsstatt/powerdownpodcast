f6e762e62edc2a7b3de74cc0596badd87831afb1
Jason Statt <js.statt@gmail.com>
2019-09-02 15:41:23 -0700

    Contact Messaging System Add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Tab
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -11,3 +12,36 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Email
+$(document).ready(function () {
+  $('.submit').click(function (event) {
+    const email = $('.email').val();
+    const subject = $('.subject').val();
+    const message = $('.message').val();
+    const status = $('.status');
+    status.empty();
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+
+    } else {
+      status.append('<div>Email not valid</div>');
+      event.preventDefault();
+    }
+
+    if (subject.length > 2) {
+
+    } else {
+      status.append('<div>Subject not valid</div>');
+      event.preventDefault();
+    }
+
+    if (message.length > 20) {
+
+    } else {
+      status.append('<div>Content not valid</div>');
+      event.preventDefault();
+    }
+
+  })
+})
